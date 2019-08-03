e6c9af68e33f8995b21a807301a4c1b66e0f5738
Matheus Nogueira Martino <matheus-martino@uergs.edu.br>
2019-08-02 23:32:51 -0300

    Update run_part.bat

--- run_part.bat
+++ run_part.bat
@@ -1,1 +1,1 @@
-pade start-runtime --username=pade_user --password=12345 --no_pade_web --no_pade_sniffer --no_pade_ams --machine_service=%1 PartAgent.py
+pade start-runtime --username=pade_user --password=12345 --no_pade_web --no_pade_sniffer --no_pade_ams --agent_name=%1 --agent_service=%2 PartAgent.py
